e0abf403fc5f55532a4016f9ecdf0177964f890c
Alexandre Fernandes Francisco <286733@contax.com.br>
2019-01-15 22:18:12 -0200

    Adjusts to decribe query

--- vw.php
+++ vw.php
@@ -1,4 +1,32 @@
 <?php
+	if (isset($_POST["win32_bios.serialnumber"])){ 
+		var_dump($_POST);
+	} else {
+		$sqlquery = "DESCRIBE wminfo";
+		//$conn = new mysqli("localhost", "root", "cascavel", "vw");
+		include 'anotheronebitesthedust.php';
+		if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+		$sqlresult = mysqli_query($conn, $sqlquery);
+
+		while($row = mysqli_fetch_array($sqlresult)){
+			echo $row['Field']."\n";
+		}
+		mysqli_close($conn);
+
+//Win32_BIOS.SerialNumber
+//Win32_NetworkAdapterConfiguration.IPAddress
+//Win32_ComputerSystem.Name
+//Win32_TimeZone.Caption
+//Win32_TimeZone.Bias
+//Win32_TimeZone.DaylightBias
+//WiN32_localtime.second
+
+	}
+?>
+
+
+
+<?php
 	/*			$reqsource = $_SERVER['REMOTE_ADDR'];
 				$sqlquery = "Select * FROM hvht WHERE hostname = '" . $_POST['hostname'] . "' AND sid = '" . $_POST['sid'] . "'";
 				$sqlinsert = "INSERT INTO hvht (hostname, ipaddress, domain, timezone, bias, dlbias, tempolocal, hvi, hvf, hvini, hvfim, so, sid, rsource) VALUES ('" . $_POST['hostname'] . "','" . $_POST['ipaddress'] . "','" . $_POST['domain'] . "','" . $_POST['timezone'] . "','" . $_POST['bias'] . "','" . $_POST['dlbias'] . "','" . $_POST['localtime'] . "','" . $_POST['hvi'] . "','" . $_POST['hvf'] . "','" . $_POST['hvini'] . "','" . $_POST['hvfim'] . "','" . $_POST['so'] . "','" . $_POST['sid'] . "','" . $reqsource ."')";
@@ -19,18 +47,4 @@
         */
 	?>
 
-<?php
 
-//$sqlquery = "SELECT * FROM hvht ORDER BY time_updated";
-$sqlquery = "DESCRIBE wminfo";
-$conn = new mysqli("localhost", "root", "cascavel", "vw");
-if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
-$sqlresult = mysqli_query($conn, $sqlquery);
-
-while($row = mysqli_fetch_array($sqlresult)){
-  echo $row['Field']."\n";
-}
-
-mysqli_close($conn);
-
-?>
